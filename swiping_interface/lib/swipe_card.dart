3ded3d9af3f021e089d02d34fbe457234b891a2e
Florian Abel <mail@florianabel.com>
2022-06-13 22:23:41 +0200

    Adding additional information to the layout

--- swiping_interface/lib/swipe_card.dart
+++ swiping_interface/lib/swipe_card.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class SwipeCard extends StatelessWidget {
   final String id;
@@ -15,7 +15,7 @@ class SwipeCard extends StatelessWidget {
     required this.userDescription,
     required this.profileImageSrc,
   }) : super(key: key);
- 
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -25,8 +25,43 @@ class SwipeCard extends StatelessWidget {
           shape: BoxShape.rectangle,
           borderRadius: BorderRadius.circular(20.0),
           image: DecorationImage(
-              image: AssetImage(profileImageSrc),
-              fit: BoxFit.cover),
+              image: AssetImage(profileImageSrc), fit: BoxFit.cover),
+        ),
+        child: Container(
+          margin: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 4.0),
+                child: Row(
+                  children: [
+                    Text(
+                      "$userName  $userAge",
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 36,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 4.0),
+                child: Text(
+                  userDescription,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    fontStyle: FontStyle.normal,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
