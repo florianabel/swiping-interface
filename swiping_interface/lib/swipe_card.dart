1f80db4c16cf3bf0ffcfa5d13786a0007cbe827d
Florian Abel <mail@florianabel.com>
2022-06-13 22:34:40 +0200

    Adding the swipe functionality to the stack of profile cards

--- swiping_interface/lib/swipe_card.dart
+++ swiping_interface/lib/swipe_card.dart
@@ -18,50 +18,48 @@ class SwipeCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
+    return Container(
+      margin: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.circular(20.0),
+        image: DecorationImage(
+            image: AssetImage(profileImageSrc), fit: BoxFit.cover),
+      ),
       child: Container(
-        margin: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
-        decoration: BoxDecoration(
-          shape: BoxShape.rectangle,
-          borderRadius: BorderRadius.circular(20.0),
-          image: DecorationImage(
-              image: AssetImage(profileImageSrc), fit: BoxFit.cover),
-        ),
-        child: Container(
-          margin: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 4.0),
-                child: Row(
-                  children: [
-                    Text(
-                      "$userName  $userAge",
-                      style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 36,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 4.0),
-                child: Text(
-                  userDescription,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                    fontStyle: FontStyle.normal,
+        margin: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 4.0),
+              child: Row(
+                children: [
+                  Text(
+                    "$userName  $userAge",
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold),
                   ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 4.0),
+              child: Text(
+                userDescription,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  fontStyle: FontStyle.normal,
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
