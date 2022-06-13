57e8d7cd39c9740ee733d282d85e4b9142636e76
Florian Abel <mail@florianabel.com>
2022-06-13 19:17:39 +0200

    Base layout

--- swiping_interface/lib/main.dart
+++ swiping_interface/lib/main.dart
@@ -7,6 +7,31 @@ class SwipeInterfaceApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Material(
+        color: Colors.transparent,
+        child: SafeArea(
+          child: Container(
+            alignment: Alignment.center,
+            color: Colors.white,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                    height: 60.0,
+                    alignment: Alignment.bottomCenter,
+                    child: null),
+                SwipeCard(),
+                Container(
+                    height: 60.0,
+                    alignment: Alignment.bottomCenter,
+                    child: null),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
